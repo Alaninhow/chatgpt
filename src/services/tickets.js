941148b67fdfd67fd22381f72d0bd4feceec3cc0
Alaninhow <neutzalan1@gmail.com>
2025-08-12 02:07:02 -0300

    Implementa sistema de ingressos com QR

--- src/services/tickets.js
+++ src/services/tickets.js
@@ -1,20 +1,11 @@
-// src/services/tickets.js
 import { httpsCallable } from 'firebase/functions';
 import { ref, getDownloadURL } from 'firebase/storage';
-import { functions, storage } from '../../firebaseConfig'; // caminho: src/services -> raiz
+import { functions, storage } from '../../firebaseConfig';
 
-/**
- * Chama a Cloud Function `createTicket` e retorna também a URL do QR no Storage.
- * Requer que você já tenha deployado a função no Firebase.
- */
 export async function createTicket({ ownerUid, type, price, validUntilMillis }) {
-  // chama a CF
   const callable = httpsCallable(functions, 'createTicket');
-  const res = await callable({ ownerUid, type, price, validUntilMillis });
-  const { ticketId, qrStoragePath } = res.data || {};
-
-  // pega URL do PNG no Storage
+  const { data } = await callable({ ownerUid, type, price, validUntilMillis });
+  const { ticketId, qrStoragePath } = data;
   const url = await getDownloadURL(ref(storage, qrStoragePath));
-
   return { ticketId, qrStoragePath, qrUrl: url };
 }
